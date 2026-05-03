243a7b9cac616a3bfb74fbaeee4f272f880363bd
Артём Каримов <grandbo@list.ru>
2026-05-03 21:35:44 +0500

    Мыши роллеры

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Super Site</h1>
-        <MyComponent/>
+        <MyComponent />
       </header>
     </div>
   );
--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,9 +1,50 @@
-import react from "react";
+import react, { useEffect, useState } from "react";
+
+const initialState = () => {
+    return -1;
+}
 
 const MyComponent = (props) =>{
+
+    const [counter, setCounter] = useState(initialState);
+    const [isOpened, setIsOpened] = useState(false);
+
+    const [x1, setX1 ] = useState(1);
+     const [x2, setX2 ] = useState(1);
+
+    const hendlerSetCounter = () => {
+        setCounter(counter + 1);
+    }
+
+const hendlerSetMouseMove = (event) => {
+    setX2(event.clientX);
+}
+
+useEffect(() => {
+    document.body.addEventListener('mousemove', (event) => {
+        setX1(window.event.clientX);
+    });
+}, [])
+
     return(
         <div>
-            
+            <button onClick={() => hendlerSetCounter()}>Click me Now!</button>
+            <button onClick={() => setIsOpened(!isOpened)}>
+                {isOpened ? 'Скрыть' : 'Плказать'}
+            </button>
+            <p>counter - {counter}</p>
+            {
+                isOpened && 
+                <div>
+                    <span>Ну что вы смотрите, прекратите!</span>
+                </div>
+            }
+            <div>
+                Координата мыши (X) - {x1}
+            </div>
+            <div onMouseMove={(event) => hendlerSetMouseMove(event)}>
+                Координата мыши (X) в области - {x2}
+            </div>
         </div>
     )
 }
