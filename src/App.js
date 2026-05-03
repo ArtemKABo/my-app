0533749986378c8b8bb8ca697c00529892887415
Артём Каримов <grandbo@list.ru>
2026-05-03 15:35:25 +0500

    бегающий рандом

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import react, { useEffect, useState } from 'react';
 
-function App() {
+function App(props) {
+
+  const [number, setNumber] = useState(0);
+  const [intnumber, setIntNumber] = useState(0);
+
+  useEffect(() => {setIntNumber(Math.floor(Math.random() * 1000))});//,[]
+  useEffect(() => {setNumber(Math.random() * 1000)});//,[]
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Заголовок - {props.title}</h1>
+        <div> oписание - {props.description}</div>
+        <h1>Новые числа!</h1>
+        <h2>целое - {intnumber}</h2>
+        <h2>всякое - {number}</h2>
       </header>
     </div>
   );
