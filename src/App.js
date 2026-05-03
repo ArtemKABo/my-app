5b0d005f6cb7b4a759e5e4d4c31671051dfcfb66
Артём Каримов <grandbo@list.ru>
2026-05-03 18:19:02 +0500

    от такого кода буду плясать

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import UserList from './UserList';
 import MyComponent from './MyComponent';
 
 function App() {
@@ -7,8 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Super Site</h1>
-        <UserList/>
-        <MyComponent prop1={'props-1'} prop2={'props-2'} />
+        <MyComponent/>
       </header>
     </div>
   );
--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,11 +1,9 @@
 import react from "react";
 
 const MyComponent = (props) =>{
-    const {prop2} = props;
     return(
         <div>
-            <p>{props.prop1}</p>
-            <p>{prop2}</p>
+            
         </div>
     )
 }
