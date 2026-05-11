91df4da87cda94bb79c4da20730f24c52667246f
Артём Каримов <grandbo@list.ru>
2026-05-11 15:49:00 +0500

    работа с апи

--- src/Contacts/Contacts.js
+++ src/Contacts/Contacts.js
@@ -1,11 +1,68 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "./Contacts.css";
 
 const Contacts = () => {
+
+    const [dataFromApi, setDataFromApi] = useState(null);
+    const [isLoaded, setIsloaded] = useState(false);
+    const [userName, setUserName] = useState("");
+    const [isDelete, setIsDelete] = useState(false);
+
+    useEffect(()=>{
+        console.log(isLoaded);
+    }, [isLoaded]);
+
+    const fetchData = (data) => {
+        fetch("https://jsonplaceholder.typicode.com/posts",{
+            method: "PUT",
+            body: JSON.stringify({
+                title: data,
+                body: "body",
+            })
+        })
+        .then((data) => data.json())
+        .then((res) => {
+            setDataFromApi(res.id);
+            setIsloaded(true);
+        });
+        
+    };
+
+    const onChangeOurInpute = (e) => {
+        if(e.target.value) {
+            setUserName(e.target.value);
+            fetchData(e.target.value);
+        }
+    };
+
+    const delUser = () => {
+        fetch(`https://jsonplaceholder.typicode.com/posts/${dataFromApi}`,{
+            method: "DELETE",
+        }).then(()=>setIsDelete(true));
+    }
+
     return(
         <div className="contact-page-container container">
             <p className="contact-page-container__head-text">
                 Contact page
                 </p>
+        <div>
+            <p>Enter your data</p>
+            <input type="text" onChange={(e) => onChangeOurInpute(e)}/>
+        </div>
+        <div>
+            {isLoaded ? <p>Ваш айди</p> : <p>Ваш айди еще не загружен</p>}
+            {isLoaded && <p>айди - {dataFromApi}</p>}
+        </div>
+        { isLoaded && (
+            <div>
+                <button onClick={() => delUser()}>
+                    {isDelete
+                    ? "Пользователь удален"
+                    : `Удалить пользователя - ${userName}`}
+                </button>
+            </div>
+        )}
         </div>
     );
 };
--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "./Home.css";
 
 const Home = () => {
+    const [dataFromApi, setDataFromApi] = useState([]);
+    useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/posts")
+        .then((result) => result.json())
+        .then((data) => setDataFromApi(data.splice(1,10)));
+    }, []);
+
     return(
-        <div className="home-page-container container">
+        <div className="home-page-container">
             <p className="home-page-container__head-text">
                 Home page
-                </p>
+            </p>
+            <p>
+                Элементы с сервера
+            </p>
+            {dataFromApi.map((el, index) => {
+                return (
+                    <div key={index + el.id} className="home-page-item">
+                        <h3 className="item-head">{el.title}</h3>
+                        <div className="item-body">{el.body}</div>
+                    </div>
+                );
+
+            })}
         </div>
     );
 };
